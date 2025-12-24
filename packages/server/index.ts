5207ae081614909352832f9b202aad9cd7f94307
fenix1409 <b2550486@gmail.com>
2025-12-24 12:19:19 +0500

    extracting chat service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
 import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -21,8 +17,6 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
 });
-
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -42,18 +36,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
+      const response = await chatService.sendMessage(prompt, conversationId);
 
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini!',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id: conversations.get(conversationId),
-      });
-
-      conversations.set(conversationId, response.id);
-
-      res.json({ message: response.output_text });
+      res.json({ message: response.message });
    } catch (err) {
       res.status(500).json({ error: 'Failed to generate a response.' });
    }
