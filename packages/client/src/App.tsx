9a01dc9263b071b6f852f6ba0e3a439992a22173
fenix1409 <b2550486@gmail.com>
2025-12-25 11:12:44 +0500

    Frontend setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((response) => response.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
-      <>
-         <p className="font-bold p-4 text-3xl">{message}</p>
-         <Button>Click me</Button>
-      </>
+      <div className="p-4">
+         <ChatBot />
+      </div>
    );
 }
 
---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,48 @@
+import { FaArrowUp } from 'react-icons/fa';
+import { Button } from './ui/button';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+   prompt: string;
+};
+
+const ChatBot = () => {
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+   const onSubmit = (data: FormData) => {
+      console.log(data);
+      reset();
+   };
+
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         handleSubmit(onSubmit)();
+      }
+   };
+   return (
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={onKeyDown}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
+         <textarea
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
+            className="w-full border-0 focus:outline-0 resize-none"
+            placeholder="Ask anything"
+            maxLength={1000}
+         />
+         <Button
+            className="rounded-full w-9 h-9"
+            disabled={formState.isSubmitting || !formState.isValid}
+         >
+            <FaArrowUp />
+         </Button>
+      </form>
+   );
+};
+
+export default ChatBot;
