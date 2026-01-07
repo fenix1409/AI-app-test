16bc3752d97c13cd0b46ea27c37022bd72e0e423
fenix1409 <b2550486@gmail.com>
2026-01-07 16:21:20 +0500

    Animation added

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,17 +1,40 @@
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
+import ReactMarkdown from 'react-markdown';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRef, useState } from 'react';
 
 type FormData = {
    prompt: string;
 };
 
+type ChatResponse = {
+   message: string;
+};
+
+type Message = {
+   content: string;
+   role: 'user' | 'bot';
+};
 const ChatBot = () => {
+   const [messages, setMessages] = useState<Message[]>([]);
+   const [isBotTyping, setIsBotTyping] = useState(false);
+   const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = async ({ prompt }: FormData) => {
+      setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
+      setIsBotTyping(true);
+
       reset();
+
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      setMessages((prev) => [...prev, { content: data.message, role: 'bot' }]);
+      setIsBotTyping(false);
    };
 
    const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
@@ -21,27 +44,50 @@ const ChatBot = () => {
       }
    };
    return (
-      <form
-         onSubmit={handleSubmit(onSubmit)}
-         onKeyDown={onKeyDown}
-         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-      >
-         <textarea
-            {...register('prompt', {
-               required: true,
-               validate: (data) => data.trim().length > 0,
-            })}
-            className="w-full border-0 focus:outline-0 resize-none"
-            placeholder="Ask anything"
-            maxLength={1000}
-         />
-         <Button
-            className="rounded-full w-9 h-9"
-            disabled={formState.isSubmitting || !formState.isValid}
+      <div>
+         <div className="flex flex-col gap-3">
+            {messages.map((message, index) => (
+               <p
+                  key={index}
+                  className={`px-3 py-1 rounded-xl ${
+                     message.role === 'user'
+                        ? 'bg-blue-600 text-white self-end'
+                        : 'bg-gray-100 text-black self-start'
+                  }`}
+               >
+                  <ReactMarkdown>{message.content}</ReactMarkdown>
+               </p>
+            ))}
+            {isBotTyping && (
+               <div className="flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl">
+                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse"></div>
+                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.2s]"></div>
+                  <div className="w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]"></div>
+               </div>
+            )}
+         </div>
+         <form
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl mt3"
          >
-            <FaArrowUp />
-         </Button>
-      </form>
+            <textarea
+               {...register('prompt', {
+                  required: true,
+                  validate: (data) => data.trim().length > 0,
+               })}
+               className="w-full border-0 focus:outline-0 resize-none"
+               placeholder="Ask anything"
+               maxLength={1000}
+            />
+            <Button
+               className="rounded-full w-9 h-9"
+               disabled={formState.isSubmitting || !formState.isValid}
+            >
+               <FaArrowUp />
+            </Button>
+         </form>
+      </div>
    );
 };
 
