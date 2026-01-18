e2c0a816b8997f57aad6909ea739368fbf5df1d7
fenix1409 <b2550486@gmail.com>
2026-01-18 12:09:11 +0500

    Prompt part finished

--- packages/server/services/chat.service.ts
+++ packages/server/services/chat.service.ts
@@ -1,9 +1,18 @@
+import fs from 'fs';
+import path from 'path';
 import OpenAI from 'openai';
 import { conversationRepository } from '../repositories/conversation.repositories';
+import templete from '../prompts/chatbot.txt';
 
 const client = new OpenAI({
    apiKey: process.env.OPENAI_API_KEY,
 });
+
+const parkInfo = fs.readFileSync(
+   path.join(__dirname, '..', 'prompts', 'WonderWorld'),
+   'utf-8'
+);
+const instructions = templete.replace('{{parkInfo}}', parkInfo);
 
 type ChatResponse = {
    id: string;
@@ -18,6 +27,7 @@ export const chatService = {
    ): Promise<ChatResponse> {
       const response = await client.responses.create({
          model: 'gpt-4o-mini',
+         instructions,
          input: prompt,
          temperature: 0.2,
          max_output_tokens: 200,
